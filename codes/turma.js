1b38ec30bfc9fda2b21ab69d7a062b22afadda7a
Caio Ornelas <caioornelasoficial@gmail.com>
2023-02-12 13:37:59 -0300

    att turma completa

--- codes/turma.js
+++ codes/turma.js
@@ -3,28 +3,4 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-/*$(document).ready(function () {
-  $(window).scroll(function () {
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
-    }
-  });
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-  });
-
-  var typed = new Typed(".typing", {
-    strings: ["Exatas"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  }); */
-
 /* menu mobile*/
