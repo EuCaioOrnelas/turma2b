6e704b1c5697e630ec9f04b116b4adc0165b9f13
Caio Ornelas <caioornelasoficial@gmail.com>
2023-02-12 23:22:47 -0300

    atualizacao login

--- codes/index.js
+++ codes/index.js
@@ -1,41 +1,14 @@
-const btnLogin = document.querySelector(".btn-login");
-const form = document.querySelector("form");
-
-btnLogin.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  const fields = [...document.querySelectorAll(".input-block input")];
-
-  fields.forEach((field) => {
-    if (field.value === "") form.classList.add("validate-error");
-  });
-
+function logar() {
+  let login = document.getElementById("email").value;
+  let senha = document.getElementById("password").value;
   const formError = document.querySelector(".validate-error");
-  if (formError) {
-    formError.addEventListener("animationend", (event) => {
-      if (event.animationName === "nono") {
-        formError.classList.remove("validate-error");
-        window.alert("Usuario ou senha, Incorretos.");
-      }
-    });
+
+  if (login == "2b@escola.pr.gov.br" && senha == "exatas123") {
+    location.href = "./src/turma.html";
   } else {
-    form.classList.add("form-hide");
-    window.location = "./src/turma.html";
+    window.alert("Usuario ou senha, Incorretos.");
   }
-});
-
-form.addEventListener("animationstart", (event) => {
-  if (event.animationName === "down") {
-    document.querySelector("body").style.overflow = "hidden";
-  }
-});
-
-form.addEventListener("animationend", (event) => {
-  if (event.animationName === "down") {
-    form.style.display = "none";
-    document.querySelector("body").style.overflow = "none";
-  }
-});
+}
 
 /* background squares */
 const ulSquares = document.querySelector("ul.squares");
